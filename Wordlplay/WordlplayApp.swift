7f78a2810a3e24b8f40a7cb9d9ada71067df2885
Ethan <ethan.scott2@icloud.com>
2023-10-13 09:30:17 -0500

    Initial Commit

---
+++ Wordlplay/WordlplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordlplayApp.swift
+//  Wordlplay
+//
+//  Created by Ethan Scott on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WordlplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
